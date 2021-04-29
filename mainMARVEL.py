3bf25aafb3612f6eb7b095682de1653ee7b4854c
jbpixel52 <50649423+jbpixel52@users.noreply.github.com>
2021-04-29 16:32:53 -0700

    fixes

--- mainMARVEL.py
+++ mainMARVEL.py
@@ -1,4 +1,4 @@
-#%%
+# %%
 import networkx as nx
 import json
 import matplotlib.pyplot as plt
@@ -7,7 +7,7 @@ print('#### PROYECTO DE ANALISIS DE ALGORITMOS ####\n')
 
 with open('./marvel.json') as marvel:
     MARVEL = json.load(marvel)
-    
+
 
 G = nx.DiGraph()
 characterlist = []
@@ -23,55 +23,58 @@ def king():
             hijos = temp
             temp = 0
             name = character['name']
-    print(name,'es quien tiene mas hijos')
+    print(name, 'es quien tiene mas hijos')
 
     return name
 
+
 def family(name):
     for character in MARVEL:
-        if character['name']==name:
-            G.add_node((character['name']),node_size=len(character['_children']))
+        if character['name'] == name:
+            G.add_node((character['name']),
+                       node_size=len(character['_children']))
             for children in character['_children']:
-                G.add_edge(character['name'],children['name'])
+                G.add_edge(character['name'], children['name'])
 
 
 def search(name):
     for node in G:
-        if node ==name:
+        if node == name:
             print(name, 'exists in G')
             return True
-    print(name,'no existe en G')
+    print(name, 'no existe en G')
     return False
 
+
 def index_character():
-    count=0
+    count = 0
     for character in MARVEL:
-        if count >50:
+        if count > 50:
             return
         characterlist.append(character)
-        count+=1
-        
+        count += 1
+
+
 def adder():
     for character in characterlist:
-        if search(character['name'])==False:
+        if search(character['name']) == False:
             family(character['name'])
 
 
 def child_to_root():
-     for node in G:
-         G.add_edge('ROOT',node)
-            
+    for node in G:
+        G.add_edge('ROOT', node)
+
+
 def main():
-    
-    
+
     index_character()
     G.add_node('ROOT', node_size=1000)
-    print('IS G TREE?',nx.is_tree(G))
+    print('IS G TREE?', nx.is_tree(G))
     adder()
     child_to_root()
-    
-    
-    nx.draw(G, node_color='r',edge_color='g',with_labels=True)
+
+    nx.draw(G, node_color='r', edge_color='g', with_labels=True)
     #nx.lowest_common_ancestor(G,'Cable','Hope Summers')
     plt.savefig('plotgraph.png', dpi=300, bbox_inches='tight')
     plt.show()
