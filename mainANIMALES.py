2cebcb006282b6ca1923725c94e317462ca5f386
McVelez <marcovelezdc@gmail.com>
2021-04-29 20:42:01 -0700

    cfvgbhnjmk

--- mainANIMALES.py
+++ mainANIMALES.py
@@ -23,11 +23,24 @@ with open('./CCNBA_Metazoa_3.json') as animales:
     metazoa = json.load(animales)
     #print(metazoa)
     Arbol = nx.Graph()
-    Arbol.add_node("Metazoa_3",size=200,node_color='y')
+
+    Arbol.add_node("Metazoa_3",size=100, node_color='y')
+
     for familia in metazoa["children"]:
         iterativeChildren(familia, Arbol, "Metazoa_3")
-    nx.draw(Arbol, node_color='r', edge_color='g', with_labels=True)
+    
+
+    color_map = []
+    for node in Arbol:
+        if node == "Metazoa_3":
+            color_map.append('blue')
+        else:
+            color_map.append('green') 
+            
+    nx.draw(Arbol, font_size=6, node_color=color_map, edge_color='g', with_labels=True)
+
+
     #nx.lowest_common_ancestor(G,'Cable','Hope Summers')
-    plt.savefig('plotgraph.png', dpi=2000, bbox_inches='tight')
+    plt.savefig('plotgraph.png', dpi=400, bbox_inches='tight')
     plt.show()
     print (matplotlib.__version__)
